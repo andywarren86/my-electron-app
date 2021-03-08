c2a14d87fd6344b1b39fb830a09aac15749759ad
Andrew <andywarren86@gmail.com>
2021-03-08 22:26:35 +1100

    Initial commit

---
+++ main.js
@@ -0,0 +1,30 @@
+/* eslint-disable import/no-extraneous-dependencies, no-console */
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+  console.log('create window');
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+
+  win.loadFile('index.html');
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
